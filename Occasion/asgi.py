35de88a1b56a02e5dea6c49c8447260bfefb905e
Anton-Krasimirov <Antonkrasimirov.com@gmail.com>
2022-11-13 00:33:05 +0200

    create project

---
+++ Occasion/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Occasion project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Occasion.settings')
+
+application = get_asgi_application()
