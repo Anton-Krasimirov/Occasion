5a090c2cd73dc729c95860533f6df85b0661e8a4
Anton-Krasimirov <Antonkrasimirov.com@gmail.com>
2022-11-13 00:42:51 +0200

    add dashboard view

--- Occasion/main/views.py
+++ Occasion/main/views.py
@@ -4,3 +4,8 @@ from django.views import generic as views
 
 class HomePageView(views.TemplateView):
     template_name = 'index.html'
+
+
+class DashboardView(views.ListView):
+    # model =
+    template_name = 'dashboard.html'
