5803d31f5c5471ea49036ff6a7fa96f7f8626f5f
Anton-Krasimirov <Antonkrasimirov.com@gmail.com>
2022-11-13 20:25:19 +0200

    added custom user moderl

--- Occasion/accounts/models.py
+++ Occasion/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import models
 
-# Create your models here.
+from Occasion.accounts.managers import OccasionUserManager
+
+
+class OccasionUser(models.AbstractBaseUser, models.PermissionsMixin):
+    email = models.EmailField(unique=True, null=False, blank=False)
+
+    is_staff = models.BooleanField(default=False, )
+
+    # is_firm = models.BooleanField(default=False, )
+
+    date_joined = models.DateTimeField(auto_now_add=True,)
+
+    USERNAME_FIELD = 'email'
+
+    objects = OccasionUserManager()
